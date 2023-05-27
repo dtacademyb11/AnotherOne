18bc6c3960c45d9957ddd2c68b33f96ce6d5eb16
Duotech B4 <duotechb3@gmail.com>
2023-05-27 12:16:01 -0400

    first

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public  class Welcome {
+        public static void main(String[] args){
+
+
+
+            File file =  new File("/Users/duotech/Desktop/First/Welcome.java");
+
+
+            System.out.println(file.exists());
+    	}
+}
